2d1e5e34c46891a3931ac1a2ce721725bd3c27c1
Michael Zentner <michael.zentner@smartbim.com>
2013-09-17 13:42:55 -0700

    added S3 test recipe

--- s3test/recipes/recipe.rb
+++ s3test/recipes/recipe.rb
@@ -1,9 +1,9 @@
 # Source accepts the protocol s3:// with the host as the bucket
 # access_key_id and secret_access_key are just that
-s3_file "/var/bulk/the_file.tar.gz" do
-source "s3://your.bucket/the_file.tar.gz"
-access_key_id your_key
-secret_access_key your_secret
+s3_file "/var/tmp/ambra.war" do
+source "s3://plos-ambra/jenkins/ambra.war"
+access_key_id "AKIAJX3TKDB6RSSOEUQQ"
+secret_access_key "bKY4Dh5DgpaJ6cqkFDSK+XOwpf/cciiT5J80/K7h"
 owner "root"
 group "root"
 mode 0644
