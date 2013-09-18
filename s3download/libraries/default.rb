df2e8c455d87bd8b9ce73016a9b1051886bacaed
Michael Zentner <mzentner-plos>
2013-09-18 12:44:45 -0700

    including s3_util directly

--- s3download/libraries/default.rb
+++ s3download/libraries/default.rb
@@ -1,7 +1,6 @@
 
-class Chef::Recipe::Namespace
-  
-  include S3Util
 
-end
+# test the connection to S3
+
+s3 =  AWS::S3.new( :access_key_id => [:aws][:acess_key] , :secret_access_key => [:secret_key] )
 
--- s3download/recipes/default.rb
+++ s3download/recipes/default.rb
@@ -1,5 +1,6 @@
 
 require 'aws/s3'
+require '../libraries/s3_util.rb'
 
 
 # download ambra related files
