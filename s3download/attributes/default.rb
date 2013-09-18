c4defd11772e93c98df066d3457b34c9ebd325d0
Michael Zentner <mzentner-plos>
2013-09-18 14:00:58 -0700

    fixed array index

--- s3download/attributes/default.rb
+++ s3download/attributes/default.rb
@@ -1,3 +1,3 @@
-default[:aws][:acess_key] = "AKIAJX3TKDB6RSSOEUQQ"
-default[:aws][:secret_key] = "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"
+default["aws"]["acess_key"] = "AKIAJX3TKDB6RSSOEUQQ"
+default["aws"]["secret_key"] = "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"
 
--- s3download/libraries/default.rb
+++ s3download/libraries/default.rb
@@ -2,5 +2,5 @@
 require 'aws/s3'
 
 # test the connection to S3
-s3 =  AWS::S3.new( :access_key_id => [:aws][:acess_key] , :secret_access_key => [:secret_key] )
+s3 =  AWS::S3.new( :access_key_id => ["aws"]["acess_key"] , :secret_access_key => ["aws"]["secret_key"] )
 
