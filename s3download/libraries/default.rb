5b50bcfbe0e543af5deafbc4b4dca40a2bc761e0
Michael Zentner <mzentner-plos>
2013-09-18 12:56:35 -0700

    including s3_util directly

--- s3download/libraries/default.rb
+++ s3download/libraries/default.rb
@@ -1,6 +1,6 @@
 
+require 'aws/s3'
 
 # test the connection to S3
-
 s3 =  AWS::S3.new( :access_key_id => [:aws][:acess_key] , :secret_access_key => [:secret_key] )
 
--- s3download/libraries/s3_util.rb
+++ s3download/libraries/s3_util.rb
@@ -1,15 +1,17 @@
+#
+# a simple util class that does a download from S3.
+#
 
 require 'aws/s3'
-
 
 module S3Util
   
   def self.download(name, bucket, dest)
   
     # establish the connection
-    
     s3 =  AWS::S3.new( :access_key_id => [:aws][:acess_key] , :secret_access_key => [:secret_key] )
 
+    # open the destination file and stream it.
     open(dest, 'w') do |file|
       s3.stream(name, bucket) do |chunk|
         file.write chunk
