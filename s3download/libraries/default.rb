943a4c9f18c058da8313b10a634f85f54c72f0d9
Michael Zentner <mzentner-plos>
2013-09-19 12:40:02 -0700

    using correct attribute syntax

--- s3download/libraries/default.rb
+++ s3download/libraries/default.rb
@@ -5,6 +5,6 @@ require 'aws/s3'
 # test the connection to S3
 #s3 =  AWS::S3.new( :access_key_id => "#{node[:aws][:acess_key]}", :secret_access_key => "#{node[:aws][:secret_key]}" )
 
-s3 =  AWS::S3.new( :access_key_id => "AKIAJX3TKDB6RSSOEUQQ", :secret_access_key => "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" )
+s3 =  AWS::S3.new( :access_key_id => "#{node[:aws][:acess_key]}", :secret_access_key => "bKY4Dh5DgpaJ6cqkFDSK+XOwpf/cciiT5J80/K7h" )
 
 
