2284ad976f58ecd63592eae67caae5bbb4bd7564
Michael Zentner <mzentner-plos>
2013-09-19 14:47:20 -0700

    attribute syntax

--- s3download/attributes/default.rb
+++ s3download/attributes/default.rb
@@ -1,7 +1,8 @@
+require 'aws/s3'
 
 # aws credentials
-default[:aws][:access_key]   = "AKIAJX3TKDB6RSSOEUQQ"
-default[:aws][:secret_key]  = "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"
+node.default[:aws][:access_key]   = "AKIAJX3TKDB6RSSOEUQQ"
+node.default[:aws][:secret_key]  = "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"
 
 # download folder
-default[:opsworks][:download_dir] = "/var/tmp/plos"
+node.default[:opsworks][:download_dir] = "/var/tmp/plos"
--- s3download/libraries/default.rb
+++ s3download/libraries/default.rb
@@ -5,6 +5,5 @@ require 'aws/s3'
 # test the connection to S3
 #s3 =  AWS::S3.new( :access_key_id => node[:aws][:access_key], :secret_access_key => node[:aws][:secret_key] )
 
-s3 =  AWS::S3.new( :access_key_id => default.node.aws.access_key, :secret_access_key => "bKY4Dh5DgpaJ6cqkFDSK+XOwpf/cciiT5J80/K7h" )
-
+access_key = node.default[:aws][:access_key]
 
