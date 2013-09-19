d9609497efa1480daf0020ae75fad8f3824dee56
Michael Zentner <mzentner-plos>
2013-09-19 13:11:28 -0700

    using correct attribute syntax

--- s3download/attributes/default.rb
+++ s3download/attributes/default.rb
@@ -1,6 +1,6 @@
 
 # aws credentials
-default[:aws][:acess_key]   = "AKIAJX3TKDB6RSSOEUQQ"
+default[:aws][:access_key]   = "AKIAJX3TKDB6RSSOEUQQ"
 default[:aws][:secret_key]  = "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"
 
 # download folder
