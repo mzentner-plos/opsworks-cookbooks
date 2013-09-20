d55c4c218859a2a1c7067bd3ba7f42d4cbe50bf9
Michael Zentner <mzentner-plos>
2013-09-20 07:50:40 -0700

    attribute syntax

--- s3download/libraries/s3_util.rb
+++ s3download/libraries/s3_util.rb
@@ -10,7 +10,7 @@ module S3Util
   def self.download(name, bucket, dest)
   
     # establish the connection
-    s3 =  AWS::S3.new( :access_key_id => "AKIAJX3TKDB6RSSOEUQQ", :secret_access_key => "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" )
+    s3 =  AWS::S3.new( :access_key_id => "#{node[:aws][:access_key]}", :secret_access_key => "#{node[:aws][:secret_key]}" )
 
     # open the destination file and stream it.
     open(dest, 'w') do |file|
