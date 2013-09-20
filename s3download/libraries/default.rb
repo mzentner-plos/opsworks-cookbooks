04c3673d920094075a5054c438fad8d91425fbd7
Michael Zentner <michael.zentner@smartbim.com>
2013-09-19 21:08:02 -0700

    working with attrs

--- s3download/libraries/default.rb
+++ s3download/libraries/default.rb
@@ -5,6 +5,6 @@ require 'aws/s3'
 # test the connection to S3
 #s3 =  AWS::S3.new( :access_key_id => node[:aws][:access_key], :secret_access_key => node[:aws][:secret_key] )
 
-access_key = #{node[:aws][:access_key]}
+access_key = "#{node[:aws][:access_key]}"
 
 
